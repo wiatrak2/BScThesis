3ecb3d7dee39abeafdafe95dde5aebae769762aa
wiatrak2 <wpratkowiecki@gmail.com>
2018-10-24 13:03:59 +0200

    zero half - building model

--- models.py
+++ models.py
@@ -65,3 +65,18 @@ class DomainPredictor(nn.Module):
 
 def extendFeatureExtractor(outputSize, inputSize=320):
 	return nn.Sequential(MnistFeatureExtractor(), nn.Linear(inputSize, outputSize))
+
+class ZeroHalf(nn.Module):
+	def __init__(self, inputSize=320, half=0, useGR=True):
+		super(ZeroHalf, self).__init__()
+		self.tensor = torch.ones(inputSize).double()
+		self.tensor[half*inputSize/2:(half+1)*inputSize/2] = 0
+		self.zeroInput = 0
+
+	def forward(self, x, lambd=1.):
+		if self.zeroInput:
+			x = x * self.tensor
+			x = grad_reverse(x, lambd)
+		self.zeroInput = 1 - self.zeroInput
+		return x.view_as(x)
+	
