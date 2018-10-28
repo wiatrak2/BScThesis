d0515506626d573cc89f4e3f217518f669f319b2
wiatrak2 <wpratkowiecki@gmail.com>
2018-10-28 19:46:45 +0100

    plot - compare different models

--- plots.py
+++ plots.py
@@ -102,4 +102,37 @@ def plot_domain_training(domain_train_history, train_history):
   axs[2].set_xlabel('batch')
   axs[2].legend(frameon=True, facecolor='white')
 
+def plot_multimodel_stats(trainers, test_histories):
+  fig, axs = plt.subplots(2, 2, figsize=(21,10))
+  fig.suptitle('Train History')
+  for trainer_num, (single_trainer, test_history) in enumerate(list(zip(trainers, test_histories))): 
+    train_history = single_trainer.train_history
 
+    epoch = np.arange(1,len(test_history['target_acc'])+1)
+    axs[0][0].plot(epoch, np.array(test_history['target_acc']), label='trainer {}'.format(trainer_num+1))
+    axs[0][0].set_title('accuracy for class prediction')
+    axs[0][0].set_ylabel('accuracy')
+    axs[0][0].set_xlabel('epoch')
+    axs[0][0].legend(frameon=True, facecolor='white')
+     
+    axs[0][1].plot(np.arange(len(train_history['train_loss'])), 
+                train_history['train_loss'], label='trainer {}'.format(trainer_num+1))
+    axs[0][1].set_title('Train loss over batches')
+    axs[0][1].set_ylabel('loss')
+    axs[0][1].set_xlabel('batch')
+    axs[0][1].legend(frameon=True, facecolor='white')
+
+    vec_len_sum = np.array(train_history['avg_len_c']) + np.array(train_history['avg_len_d'])
+    axs[1][0].plot(np.arange(len(train_history['avg_len_c'])), vec_len_sum,
+                   label='trainer {}'.format(trainer_num+1))
+    axs[1][0].set_title('Avg len of vectors')
+    axs[1][0].set_ylabel('len of vector')
+    axs[1][0].set_xlabel('batch')
+    axs[1][0].legend(frameon=True, facecolor='white')
+
+    axs[1][1].plot(np.arange(len(train_history['avg_dot'])), 
+                train_history['avg_dot'], label='trainer {}'.format(trainer_num+1))
+    axs[1][1].set_title('Avg of dot product over batches')
+    axs[1][1].set_ylabel('avg of dot')
+    axs[1][1].set_xlabel('batch')
+    axs[1][1].legend(frameon=True, facecolor='white')
