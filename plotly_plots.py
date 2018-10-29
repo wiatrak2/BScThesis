8c632528584515d210b3e382649255a2edb2ac6a
wiatrak2 <wpratkowiecki@gmail.com>
2018-10-29 15:33:19 +0100

    new plotly subplots

--- plotly_plots.py
+++ plotly_plots.py
@@ -27,6 +27,20 @@ def plot_multimodel_stats(trainers, test_histories):
 		showlegend= False
 	) for i in range(len(test_histories))]
 
+	trace_source_acc = [go.Scatter(
+		x = epoch,
+		y = test_histories[i]['source_acc'],
+		name = 'model {}'.format(i),
+		showlegend= False
+	) for i in range(len(test_histories))]
+
+	trace_domain_acc = [go.Scatter(
+		x = epoch,
+		y = test_histories[i]['domain_acc'],
+		name = 'model {}'.format(i),
+		showlegend= False
+	) for i in range(len(test_histories))]
+
 	trace_loss = [go.Scatter(
 		x = np.arange(len(trainers[i].train_history['train_loss'])),
 		y = trainers[i].train_history['train_loss'],
@@ -48,12 +62,16 @@ def plot_multimodel_stats(trainers, test_histories):
 		showlegend= False
 	) for i in range(len(trainers))]
 
-	fig = tools.make_subplots(rows=2, cols=2)
-	for (t_acc, t_loss, t_len, t_dot) in list(zip(trace_acc, trace_loss, trace_len, trace_dot)):
+	fig = tools.make_subplots(rows=3, cols=2)
+	for (t_acc, t_source, t_domain, t_loss, t_len, t_dot) in /
+	list(zip(trace_acc, trace_source_acc, trace_domain_acc, trace_loss, trace_len, trace_dot)):
 		fig.append_trace(t_acc, 1, 1)
-		fig.append_trace(t_loss, 1, 2)
-		fig.append_trace(t_len, 2, 1)
-		fig.append_trace(t_dot, 2, 2)
+		fig.append_trace(t_source, 1, 2)
+		fig.append_trace(t_domain, 2, 1)
+		fig.append_trace(t_loss, 2, 2)
+		fig.append_trace(t_len, 3, 1)
+		fig.append_trace(t_dot, 3, 2)
+		
 
 	configure_plotly_browser_state()
 
