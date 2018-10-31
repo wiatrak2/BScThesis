54e6e151e03cdc600b9e3399b681e6e1db7a8be8
wiatrak2 <wpratkowiecki@gmail.com>
2018-10-31 22:16:23 +0100

    preparing to new GR domain predictor plugging

--- models.py
+++ models.py
@@ -29,11 +29,11 @@ class MnistFeatureExtractor(nn.Module):
 	def forward(self, x):
 		x = F.leaky_relu(F.max_pool2d(self.conv1(x), 2))
 		x = F.leaky_relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-		return x.view(-1, 320)
+		return x.view(x.size(0), -1)
 
 
 class MnistClassPredictor(nn.Module):
-	def __init__(self, input_size=320, inner_size=50):
+	def __init__(self, input_size=320, inner_size=100):
 		super(MnistClassPredictor, self).__init__()
 		self.fc1 = nn.Linear(input_size, inner_size)
 		self.fc2 = nn.Linear(inner_size, 10)
@@ -78,34 +78,38 @@ class DomainPredictor(nn.Module):
 		x = self.fc2(x)
 		return F.log_softmax(x, dim=1)
 
-class ZeroHalf(nn.Module):
-	def __init__(self, device, output_model, tensor_size, half=0):
-		super(ZeroHalf, self).__init__()
-		self.tensor = torch.ones(tensor_size).to(device)
-		if half == 0:
-			self.tensor[:tensor_size//2] = 0
-		else:
-			self.tensor[tensor_size//2:] = 0
+class CutHalf(nn.Module):
+	def __init__(self, output_model, half=0):
+		super(CutHalf, self).__init__()
+		self.half = half
 		self.output_model = output_model
 		
 	def get_mtx(self):
 		return self.output_model.get_mtx()
 	
 	def forward(self, x, *args):
-		x *= self.tensor
+		x = x[:, :x.size()[1]//2] if self.half == 0 else x[:, x.size()[1]//2:]
 		if args:
-			return self.output_model(x, args)
+			return self.output_model(x, *args)
 		return self.output_model(x)
 
 class LinearFromList(nn.Module):
 	def __init__(self, size_list, use_gr=False, output_model=None):
 		super(LinearFromList, self).__init__()
+		self.size_list = size_list
 		self.linears = nn.ModuleList([nn.Linear(size_list[i], size_list[i+1]) for i in range(len(size_list)-1)])
 		self.output_model = output_model
 		self.use_gr = use_gr
 
 	def get_mtx(self):
 		return self.linears[0] if len(self.linears) > 0 else self.output_model.get_mtx()
+
+	def split(self, split_after):
+		if split_after > len(self.linears):
+			return self, None
+		new_model = LinearFromList(self.size_list[:split_after+1], use_gr=self.use_gr)
+		new_model_out = LinearFromList(self.size_list[split_after:], use_gr=self.use_gr, output_model=self.output_model)
+		return new_model, new_model_out
 
 	def forward(self, x, lambd=1.):
 		if self.use_gr:
@@ -117,14 +121,20 @@ class LinearFromList(nn.Module):
 			return self.output_model(x)
 		return x
 
-def extend_feature_extractor(output_size, input_size=320):
-	return nn.Sequential(MnistFeatureExtractor(), nn.Linear(input_size, output_size))
+def extend_feature_extractor(model_f, model_continuation, return_size=True):
+	for param in model_f.parameters():
+		param.requires_grad = False
+	new_model_f = nn.Sequential(model_f, model_continuation)
+	if return_size:
+		return new_model_f, model_continuation.get_mtx().in_features
+	return new_model_f
+
+
 
 def get_models(model_f_linear, model_c_linear, model_d_linear, use_gr=False, model_f_dropout=False):
 	model_f = nn.Sequential(MnistFeatureExtractor(), LinearFromList(model_f_linear))
 	if not model_f_dropout:
 		model_f = nn.Sequential(model_f, nn.Linear(model_f_linear[-1], model_f_linear[-1]))
-	model_c = nn.Sequential(LinearFromList(model_c_linear), nn.Linear(model_c_linear[-1], 10), nn.LogSoftmax(dim=1))
-	model_c.get_mtx = lambda: model_c[0].linears[0]
+	model_c = LinearFromList(model_c_linear, output_model=nn.Sequential(nn.Linear(model_c_linear[-1], 10), nn.LogSoftmax(dim=1)))
 	model_d = LinearFromList(model_d_linear[:-1], use_gr, output_model=DomainPredictor(model_d_linear[-2], model_d_linear[-1]))
 	return model_f, model_c, model_d
